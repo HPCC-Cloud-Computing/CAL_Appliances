800d6fb480c3ba986441ca501971cc36f3127a83
Kien Nguyen <ntk148v@gmail.com>
2017-03-31 16:35:14 +0700

    Create mcs/interface branch

--- MCS/mcs/apps/authentication/models.py
+++ MCS/mcs/apps/authentication/models.py
@@ -1,5 +1,34 @@
 from __future__ import unicode_literals
+import os
 
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+CURRENT_DIR = os.path.dirname(__file__)
+IMG_DIR = os.path.join(CURRENT_DIR, 'static/assets/img/')
+
+
+class UserProfile(models.Model):
+    class Meta:
+        db_table = 'userprofile'
+        app_label = 'authentication'
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='profile')
+    company = models.CharField(default='HUST', max_length=50)
+    image = models.ImageField()
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- MCS/mcs/apps/dashboard/urls.py
+++ MCS/mcs/apps/dashboard/urls.py
@@ -1,6 +1,33 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
+from mcs.apps.dashboard.views import files, user
+
 urlpatterns = [
-    url(r'^test/', TemplateView.as_view(template_name='dashboard/base.html')),
+    url(r'^home/$',
+        TemplateView.as_view(template_name='dashboard/home.html'),
+        name='home'),
+    url(r'^clouds/$',
+        TemplateView.as_view(template_name='dashboard/clouds.html'),
+        name='clouds'),
+    url(r'^files/$',
+        files.list_files,
+        name='files'),
+    url(r'^files/(?P<folder_id>\d+)/upload/$',
+        files.upload_file, name='upload_file'),
+    url(r'^files/upload/$',
+        files.upload_file, name='upload_root_file'),
+    url(r'^files/(?P<folder_id>\d+)/create/$',
+        files.create_folder, name='create_folder'),
+    url(r'^files/create/$',
+        files.create_folder, name='create_root_folder'),
+    url(r'^files/delete/$',
+        files.delete_files, name='delete_files'),
+    url(r'^files/download/$',
+        files.download_file, name='download_file'),
+    url(r'^settings/$',
+        TemplateView.as_view(template_name='dashboard/settings.html'),
+        name='settings'),
+    url(r'^user/$',
+        user.update_user, name='user')
 ]
